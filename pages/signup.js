88d57743161dc7d3e1fc91b3704a56c3d44ee860
Prince Kumar <princek9576@gmail.com>
2021-07-26 21:07:58 +0530

    Signup and login page markkup completed

--- pages/signup.js
+++ pages/signup.js
@@ -2,15 +2,10 @@ import {
   HeaderMessage,
   FooterMessage,
 } from "../components/Common/WelcomeMessage";
-import {
-  Form,
-  Button,
-  Message,
-  Textarea,
-  Segment,
-  Divider,
-} from "semantic-ui-react";
-import { useState } from "react";
+import { Form, Button, Message, Segment, Divider } from "semantic-ui-react";
+import { useEffect, useRef, useState } from "react";
+import SocialLinks from "../components/Common/SocialLinks";
+import ImageDragger from "../components/Common/ImageDragger";
 
 const regexUserName = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/;
 
@@ -31,17 +26,36 @@ const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [errorMsg, setErrorMsg] = useState(null);
   const [formLoading, setFormLoading] = useState(false);
+  const [submitDisabled, setSubmitDisabled] = useState(true);
 
   const [username, setUsername] = useState("");
   const [usernameLoading, setUsernameLoading] = useState(false);
   const [usernameAvailable, setUsernameAvailable] = useState(false);
 
+  const [media, setMedia] = useState(null);
+  const [mediaPreview, setMediaPreview] = useState(null);
+  const [highlighted, setHighlighted] = useState(false);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    const isValid = Object.values({ name, email, password, bio }).every(
+      (value) => {
+        return value;
+      }
+    );
+    isValid ? setSubmitDisabled(false) : setSubmitDisabled(true);
+  }, [user]);
   const inputChangeHandler = (event) => {
     const { name, value } = event.target;
     setUser((prevState) => {
       return { ...prevState, [name]: value };
     });
     console.log(event);
+  };
+  const fileChangeHandler = (event) => {
+    const files = event.target.files;
+    setMedia(files[0]);
+    setMediaPreview(URL.createObjectURL(files[0]));
   };
   const handleFormSubmit = (event) => event.preventDefault();
   return (
@@ -59,10 +73,21 @@ const Signup = () => {
           onDismiss={() => setErrorMsg(null)}
         />
         <Segment>
+          <ImageDragger
+            media={media}
+            mediaPreview={mediaPreview}
+            setMedia={setMedia}
+            setMediaPreview={setMediaPreview}
+            inputRef={inputRef}
+            highlighted={highlighted}
+            setHighlighted={setHighlighted}
+            fileChangeHandler={fileChangeHandler}
+          />
           <Form.Input
             required
             fluid
             name="name"
+            error={!name}
             value={name}
             label="Name"
             icon="user"
@@ -74,6 +99,7 @@ const Signup = () => {
             required
             fluid
             name="email"
+            error={!email}
             value={email}
             label="Email"
             icon="envelope"
@@ -88,6 +114,7 @@ const Signup = () => {
             name="password"
             value={password}
             label="Password"
+            error={!password}
             icon={{
               name: "eye",
               circular: true,
@@ -120,8 +147,25 @@ const Signup = () => {
               }
             }}
           />
+
+          <SocialLinks
+            user={user}
+            inputChangeHandler={inputChangeHandler}
+            socialLinks={socialLinks}
+            setSocialLinks={setSocialLinks}
+          />
+          <Divider hidden />
+          <Button
+            fluid
+            color="green"
+            type="submit"
+            content="Signup"
+            size="large"
+            disabled={submitDisabled || !usernameAvailable}
+          />
         </Segment>
       </Form>
+      <Divider hidden />
       <FooterMessage />
     </div>
   );
