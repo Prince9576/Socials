a1136cf5b2b78b65cea7d017c308334ea017967e
Prince Kumar <princek9576@gmail.com>
2021-08-10 23:22:53 +0530

    Added auth middleware

--- api/login.js
+++ api/login.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const UserModel = require("../models/UserModel");
+const FollowerModel = require("../models/FollowerModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const isEmail = require("validator/lib/isEmail");
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.get("/", authMiddleware, async (req, res) => {
+  const userId = req.userId;
+  try {
+    const user = await UserModel.findById(userId);
+    const followerStats = await FollowerModel.findOne({ user: userId });
+    return res.status(200).json({
+      user,
+      followerStats,
+    });
+  } catch (err) {
+    console.error("Error getting auth", err);
+    return res.status(500).send("Internal Server Error");
+  }
+});
 
 router.post("/", async (req, res) => {
   const { email, password } = req.body.user;
