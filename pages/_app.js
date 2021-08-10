1b891cd2446f9a70c43026af8137030301d0856f
Prince Kumar <princek9576@gmail.com>
2021-08-10 23:55:26 +0530

    Get initial prop added

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import App from "next/app";
 import Layout from "../components/Layout/Layout";
 import "semantic-ui-css/semantic.min.css";
+import axios from "axios";
+import { redirectUser } from "../utils/authUser";
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return pageProps;
+  }
   render() {
     const { Component } = this.props;
     return (
-      <Layout>
-        <Component></Component>
+      <Layout {...pageProps}>
+        <Component {...pageProps}></Component>
       </Layout>
     );
   }
