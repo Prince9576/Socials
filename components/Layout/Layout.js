334af50c717c357f7e1b52003c47f38a323a8018
Prince Kumar <princek9576@gmail.com>
2021-08-31 00:09:39 +0530

    Removed Navbar

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,6 +1,5 @@
 import React, { createRef, Fragment } from "react";
 import HeadTags from "./HeadTags";
-import Navbar from "./Navbar";
 import Router from "next/router";
 import nProgress from "nprogress";
 import {
@@ -22,31 +21,23 @@ function Layout({ children, user }) {
   return (
     <>
       <HeadTags />
-      {user && (
-        <div style={{ padding: "1rem", margin: "0 auto", width: "70%" }}>
-          <Ref innerRef={contextRef}>
-            <Grid>
-              <Grid.Column floated="left" width={4}>
-                <Sticky context={contextRef}>
-                  <SideMenu user={user} />
-                </Sticky>
-              </Grid.Column>
 
-              <Grid.Column width={12}>
-                <Visibility context={contextRef}>{children}</Visibility>
-              </Grid.Column>
-            </Grid>
-          </Ref>
-        </div>
-      )}
-      {!user && (
-        <Fragment>
-          <Navbar />
-          <div style={{ paddingTop: "2rem", fontSize: "1.2rem" }}>
-            {children}
-          </div>
-        </Fragment>
-      )}
+      <div style={{ padding: "1rem", margin: "0 auto", width: "70%" }}>
+        <Ref innerRef={contextRef}>
+          <Grid>
+            <Grid.Column floated="left" width={4}>
+              <Sticky context={contextRef}>
+                <SideMenu user={user} />
+              </Sticky>
+            </Grid.Column>
+
+            <Grid.Column width={12}>
+              {user && <Visibility context={contextRef}>{children}</Visibility>}
+              {!user && <div style={{ fontSize: "1.2rem" }}>{children}</div>}
+            </Grid.Column>
+          </Grid>
+        </Ref>
+      </div>
     </>
   );
 }
--- components/Layout/SideMenu.js
+++ components/Layout/SideMenu.js
@@ -6,99 +6,146 @@ import { List, Icon } from "semantic-ui-react";
 import { Fragment } from "react";
 import { logoutUser } from "../../utils/authUser";
 
-const SideMenu = ({
-  user: { unreadMessage, unreadNotification, email, username },
-}) => {
+const SideMenu = (props) => {
+  let unreadMessage, unreadNotification, email, username;
   const router = useRouter();
   const isActive = (route) => router.pathname === route;
+  if (props.user) {
+    unreadMessage = props.user.unreadMessage;
+    unreadNotification = props.user.unreadNotification;
+    email = props.user.email;
+    username = props.user.username;
+  }
   return (
     <Fragment>
       <List size="big" verticalAlign="middle" selection>
-        <Link href="/">
-          <List.Item className={styles.item} active={isActive("/")}>
-            <Icon name="home" size="large" color={isActive("/") && "teal"} />
-            <List.Content>
-              <List.Header
-                className={styles.header}
-                content="Home"
-              ></List.Header>
-            </List.Content>
-          </List.Item>
-        </Link>
+        {props.user && (
+          <>
+            <Link href="/">
+              <List.Item className={styles.item} active={isActive("/")}>
+                <Icon
+                  name="home"
+                  size="large"
+                  color={isActive("/") && "teal"}
+                />
+                <List.Content>
+                  <List.Header
+                    className={styles.header}
+                    content="Home"
+                  ></List.Header>
+                </List.Content>
+              </List.Item>
+            </Link>
+            <Link href="/messages">
+              <List.Item className={styles.item} active={isActive("/messages")}>
+                <Icon
+                  name={unreadMessage ? "hand point right" : "mail outline"}
+                  size="large"
+                  color={
+                    (isActive("/messages") && "teal") ||
+                    (unreadMessage && "orange")
+                  }
+                />
+                <List.Content>
+                  <List.Header
+                    className={styles.header}
+                    content="Messages"
+                  ></List.Header>
+                </List.Content>
+              </List.Item>
+            </Link>
+            <Link href="/notifications">
+              <List.Item
+                className={styles.item}
+                active={isActive("/notifications")}
+              >
+                <Icon
+                  name={
+                    unreadNotification ? "hand point right" : "bell outline"
+                  }
+                  size="large"
+                  color={
+                    (isActive("/notifications") && "teal") ||
+                    (unreadNotification && "orange")
+                  }
+                />
+                <List.Content>
+                  <List.Header
+                    className={styles.header}
+                    content="Notifications"
+                  ></List.Header>
+                </List.Content>
+              </List.Item>
+            </Link>
+            <Link href={`/${username}`}>
+              <List.Item
+                className={styles.item}
+                active={router.query.username === username}
+              >
+                <Icon
+                  name="user"
+                  size="large"
+                  color={router.query.username === username && "teal"}
+                />
+                <List.Content>
+                  <List.Header
+                    className={styles.header}
+                    content="Account"
+                  ></List.Header>
+                </List.Content>
+              </List.Item>
+            </Link>
+            <List.Item
+              onClick={() => {
+                logoutUser(email);
+              }}
+              className={styles.item}
+            >
+              <Icon name="log out" size="large" />
+              <List.Content>
+                <List.Header
+                  className={styles.header}
+                  content="Logout"
+                ></List.Header>
+              </List.Content>
+            </List.Item>{" "}
+          </>
+        )}
 
-        <Link href="/messages">
-          <List.Item className={styles.item} active={isActive("/messages")}>
-            <Icon
-              name={unreadMessage ? "hand point right" : "mail outline"}
-              size="large"
-              color={
-                (isActive("/messages") && "teal") || (unreadMessage && "orange")
-              }
-            />
-            <List.Content>
-              <List.Header
-                className={styles.header}
-                content="Messages"
-              ></List.Header>
-            </List.Content>
-          </List.Item>
-        </Link>
-
-        <Link href="/notifications">
-          <List.Item
-            className={styles.item}
-            active={isActive("/notifications")}
-          >
-            <Icon
-              name={unreadNotification ? "hand point right" : "bell outline"}
-              size="large"
-              color={
-                (isActive("/notifications") && "teal") ||
-                (unreadNotification && "orange")
-              }
-            />
-            <List.Content>
-              <List.Header
-                className={styles.header}
-                content="Notifications"
-              ></List.Header>
-            </List.Content>
-          </List.Item>
-        </Link>
-
-        <Link href={`/${username}`}>
-          <List.Item
-            className={styles.item}
-            active={router.query.username === username}
-          >
-            <Icon
-              name="user"
-              size="large"
-              color={router.query.username === username && "teal"}
-            />
-            <List.Content>
-              <List.Header
-                className={styles.header}
-                content="Account"
-              ></List.Header>
-            </List.Content>
-          </List.Item>
-        </Link>
-
-        <List.Item
-          onClick={() => {
-            logoutUser(email);
-          }}
-          className={styles.item}
-        >
-          <Icon name="log out" size="large" />
-          <List.Content>
-            <List.Header
-              className={styles.header}
-              content="Logout"
-            ></List.Header>
-          </List.Content>
-        </List.Item>
+        {!props.user && (
+          <>
+            <Link href="/signup">
+              <List.Item className={styles.item} active={isActive("/signup")}>
+                <Icon
+                  name="signup"
+                  size="large"
+                  color={isActive("/signup") && "teal"}
+                />
+                <List.Content>
+                  <List.Header
+                    className={styles.header}
+                    content="Signup"
+                  ></List.Header>
+                </List.Content>
+              </List.Item>
+            </Link>
+            <Link href="/login">
+              <List.Item className={styles.item} active={isActive("/login")}>
+                <Icon
+                  name="sign in"
+                  size="large"
+                  color={isActive("/login") && "teal"}
+                />
+                <List.Content>
+                  <List.Header
+                    className={styles.header}
+                    content="Login"
+                  ></List.Header>
+                </List.Content>
+              </List.Item>
+            </Link>{" "}
+          </>
+        )}
       </List>
     </Fragment>
   );
